e7646672b30b02d51751513ccecb3a12ab7f3f60
Sam French <sfrench_01@yahoo.co.uk>
2012-09-06 14:10:31 +0100

    Adding sample work so far for eventmvc

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/events', routes.get_events);
+app.get('/articles', routes.articles);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,23 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
+exports.articles = function(req, res){
+  var request = restler.get("http://juicer.responsivenews.co.uk/events.json");
+
+    request.on('complete', function(result) {
+      if (result instanceof Error) {
+        console.log('Error: ' + result.message);
+      } else {
+        console.log(result);
+
+        res.render('articles', {
+          title: 'Articles',
+          data: JSON.stringify(result)
+        });
+      }
+    });
+};
+
 exports.get_events = function(req, res){
     var request = restler.get("http://juicer.responsivenews.co.uk/events.json");
 
